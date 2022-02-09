fa8c0bc3fdf12cb445057000b7cae2304021a0ae
lina <97695274+aevlg@users.noreply.github.com>
2022-02-09 17:17:05 +0400

    commit

--- src/HomeWorkApp7/GameField.java
+++ src/HomeWorkApp7/GameField.java
@@ -2,11 +2,327 @@ package HomeWorkApp7;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.Random;
+
 
 public class GameField extends JPanel {
 
-    GameField(){
-        setBackground(new Color(60, 136, 194)); // фон поля
+    private Random random = new Random();
+
+    private GameWindow gameWindow;
+
+    //поле
+    private int fieldSizeX;
+    private int fieldSizeY;
+    private int[][] field;
+    private int seriesWin; // кол-во ячеек для победы
+
+
+    // игроки
+
+    private final int player = 1;
+    private final int pc = 2;
+    private final int  empty = 0;
+
+
+
+    private final int STATE_HUMAN_WIN = 1;
+    private final int STATE_AI_WIN = 2;
+    private final int STATE_DRAW = 3;
+
+
+
+    //сообщения и состояния
+    private final String MSG_HUMAN_WIN = "ТЫ ВЫИГРАЛ!";
+    private final String MSG_AI_WIN = "ПОРАЖЕНИЕ!";
+    private final String MSG_DRAW = "НИЧЬЯ!";
+
+    private int cellWidth;
+    private int cellHeight;
+
+    private boolean isExistGameField;
+    private boolean isGameOver;
+
+
+    GameField(GameWindow gameWindow) {
+        this.gameWindow = gameWindow;
+        setBackground(new Color(183, 239, 207));
+
+
+        //клик мышки
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseReleased(MouseEvent mouse) {
+                super.mouseReleased(mouse);
+                updater(mouse);
+            }
+        });
+
+        isExistGameField = false; //начало игры
+        isGameOver = true;
+    }
+
+
+
+    //поле
+    void startGame(int fieldSizeX, int fieldSizeY, int seriesWin) {
+        this.fieldSizeX = fieldSizeX;
+        this.fieldSizeY = fieldSizeY;
+        this.seriesWin = seriesWin;
+
+        isExistGameField = true;
+        isGameOver = false;
+
+        field = new int[fieldSizeY][fieldSizeX];
+        repaint(); //обновить состояние
 
     }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        render(g);
+    }
+    // отрисовка
+    private void render(Graphics g) {
+        if (!isExistGameField) {
+            return;
+        }
+
+        paintGameMap(g);
+
+        for (int y = 0; y < fieldSizeY; y++) {
+            for (int x = 0; x < fieldSizeX; x++) {
+
+                if (checkEmptyFieldCell(x, y)) {
+                    continue;
+                }
+
+                if (field[y][x] == player) {
+                    g.setColor(Color.BLUE);
+                    g.fillRect(x * cellWidth + 10, y * cellHeight + 10,
+                            cellWidth - 20, cellHeight - 20);
+                }
+
+                if (field[y][x] == pc) {
+                    g.setColor(Color.PINK);
+                    g.fillRect(x * cellWidth + 10, y * cellHeight + 10,
+                            cellWidth - 20, cellHeight - 20);
+                }
+            }
+        }
+
+        if (isGameOver) {
+            return;
+        }
+    }
+    //отрисовка кликов
+    private void updater(MouseEvent mouse) {
+
+        if (isGameOver) {
+            return;
+        }
+
+        if (!isExistGameField) {
+            return;
+        }
+
+        int clickX = mouse.getX() / cellWidth;
+        int clickY = mouse.getY() / cellHeight;
+
+        if (!checkTurnPlayer(clickX, clickY) || !checkEmptyFieldCell(clickX, clickY)) {
+            return;
+        }
+
+        field[clickY][clickX] =player;
+        gameWindow.saveLog(" ТВОЙ ХОД  В ЯЧЕЙКУ > [" + (clickX + 1) + ":" + (clickY + 1) + "]");
+
+        if (checkWin(player)) {
+            setGameOver(STATE_HUMAN_WIN);
+            return;
+        }
+
+        if (checkFullField()) {
+            setGameOver(STATE_DRAW);
+            return;
+        }
+
+        pcTurn();
+        repaint();
+
+        if (checkWin(pc)) {
+            setGameOver(STATE_AI_WIN);
+            return;
+        }
+
+        if (checkFullField()) {
+            setGameOver(STATE_DRAW);
+            return;
+        }
+    }
+
+    private void setGameOver(int state) {
+        repaint();
+        isGameOver = true;
+
+        switch (state) {
+            case STATE_HUMAN_WIN:
+                gameWindow.saveLog(MSG_HUMAN_WIN);
+                JOptionPane.showMessageDialog(this, MSG_HUMAN_WIN); //всплывающее окно
+                break;
+            case STATE_AI_WIN:
+                gameWindow.saveLog(MSG_AI_WIN);
+                JOptionPane.showMessageDialog(this, MSG_AI_WIN);
+                break;
+            case STATE_DRAW:
+                gameWindow.saveLog(MSG_DRAW);
+                JOptionPane.showMessageDialog(this, MSG_DRAW);
+                break;
+        }
+    }
+
+
+     //отрисовка поля
+    private void paintGameMap(Graphics g) {
+        int width = getWidth();
+        int height = getHeight();
+
+        cellWidth = width / fieldSizeX;
+        cellHeight = height /fieldSizeY;
+
+        g.setColor(new Color(0, 0, 0));
+
+        for (int i = 0; i <= fieldSizeY; i++) {
+            int y = i * cellHeight;
+            g.drawLine(0, y, width, y); //нарисовать линию
+        }
+//горизонт
+        for (int i = 0; i <= fieldSizeX; i++) {
+            int x = i * cellWidth;
+            g.drawLine(x, 0, x, height); //нарисовать линию
+        }
+    }
+
+
+    // ход пк
+    private void  pcTurn() {
+        if (turnOWinCell()) {
+            return;
+        }
+        if (turnXWinCell()) {
+            return;
+        }
+        int x;
+        int y;
+        do {
+            x = random.nextInt(fieldSizeX);
+            y = random.nextInt(fieldSizeY);
+        } while (!checkEmptyFieldCell(x, y));
+        field[y][x] = pc;
+        gameWindow.saveLog("ХОД ИГРОКА В ЯЧЕЙКУ > [" + (x + 1) + ":" + (y + 1) + "]");
+    }
+
+    //ии
+    private boolean turnOWinCell() {
+        for (int i = 0; i < fieldSizeY; i++) {
+            for (int j = 0; j < fieldSizeX; j++) {
+                if (checkEmptyFieldCell(j, i)) {
+                    field[i][j] = pc;
+                    if (checkWin(pc)) {
+                        gameWindow.saveLog("AI turn in [" + (j + 1) + ":" + (i + 1) + "]");
+                        return true;
+                    }
+                    field[i][j] = empty;
+                }
+            }
+        }
+        return false;
+    }
+    private  boolean turnXWinCell() {
+        for (int i = 0; i < fieldSizeY; i++) {
+            for (int j = 0; j < fieldSizeX; j++) {
+                if (checkEmptyFieldCell(j, i)) {
+                    field[i][j] = player;
+                    if (checkWin(player)) {
+                        field[i][j] = pc;
+                        gameWindow.saveLog("AI turn in [" + (j + 1) + ":" + (i + 1) + "]");
+                        return true;
+                    }
+                    field[i][j] = empty;
+                }
+            }
+        }
+        return false;
+    }
+
+
+    // проверка на победу
+    private  boolean checkWin(int checkChar) {
+        for (int i = 0; i < fieldSizeX; i++) {
+            for (int j = 0; j < fieldSizeY; j++) {
+                // x
+                if (checkVector(i, j, 1, 0, seriesWin, checkChar)) {
+                    return true;
+                }
+                //x y
+                if (checkVector(i, j, 1, 1, seriesWin, checkChar)) {
+                    return true;
+                }
+                // y
+                if (checkVector(i, j, 0, 1, seriesWin, checkChar)) {
+                    return true;
+                }
+                //x -y
+                if (checkVector(i, j, 1, -1, seriesWin, checkChar)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    //  линии
+    private  boolean checkVector(int x, int y, int vx, int vy, int len, int checkChar) {
+        // конец линии
+        int farX = x + (len - 1) * vx;
+        int farY = y + (len - 1) * vy;
+
+        if (!checkTurnPlayer(farX, farY)) {
+            return false;
+        }
+        // проверка одинаковых символов в ячейках
+        for (int i = 0; i < len; i++) {
+            if (field[y + i * vy][x + i * vx] != checkChar) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private  boolean checkFullField() {
+        for (int i = 0; i < fieldSizeY; i++) {
+            for (int j = 0; j < fieldSizeX; j++) {
+                if (field[i][j] == empty) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    //за пределы поля
+    private  boolean checkTurnPlayer(int x, int y) {
+        return x >= 0 && x < fieldSizeX && y >= 0 && y < fieldSizeY;
+    }
+
+    // пустая ячейка
+    private  boolean checkEmptyFieldCell(int x, int y) {
+        return field[y][x] == empty;
+    }
+
+
 }
+
+
--- src/HomeWorkApp7/GameWindow.java
+++ src/HomeWorkApp7/GameWindow.java
@@ -1,9 +1,14 @@
 package HomeWorkApp7;
 
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class GameWindow  extends JFrame {
+public class GameWindow extends JFrame {
+
     //размеры окна
     private int winWidht;
     private int winHeight;
@@ -17,82 +22,161 @@ public class GameWindow  extends JFrame {
     private JButton buttonStartGame;
     private JButton buttonEndGame;
 
-    private GameField gameField; // игровое поле
+    private JButton buttonClearLog; //очистка
+
+
+    private GameField gameField;// игровое поле
 
     private JPanel panelSettings; // панель настроек общий
-
     private JPanel panelControls; // панелька для настроек
+    // кол-во ячеек для победы
+    private JSlider sliderMapSize;
 
-    GameWindow(){
-        // настройки окна
-        prepareWindow();
-        // панельки
-        prepareGameSettingsPanel();
+
+
+    private JLabel labelMapSize;
+    private final String MAP_SIZE_PREFIX = "РАЗМЕР ИГВОГО ПОЛЯ: ";
+
+    // бегунки
+    private JSlider sliderWinLength;
+    private JLabel labelWinLength;
+    private final String WIN_LENGTH_PREFIX = "КОЛЛИЧЕСТВО ЯЧЕЕК ДЛЯ ПОБЕДЫ:  ";
+
+
+    //минимальный и максимальный размер карты
+    private int minMapSize = 3;
+    private int minWinLength = 3; // ячеек для победы
+    private int maxMapSize = 10;
+
+    private JTextArea gameLog;
+    private JScrollPane scrollPanel; // скрол
+
+
+    GameWindow() {
+        prepareWindow(); // настройки окна
+        prepareGameSettingsPanel(); //панель
         // кнопки
         prepareGameButtons();
-
         // область для кнопок
         prepareGameControlsPanel();
 
-        setVisible(true); // видимость окна
 
+        prepareGameLog(); //панель с текстом
 
-
-
-        gameField = new GameField();
+        gameField = new GameField(this);
         panelSettings.add(panelControls, BorderLayout.NORTH);
+        panelSettings.add(scrollPanel, BorderLayout.SOUTH);
+
         add(panelSettings, BorderLayout.EAST);
         add(gameField);
 
-
-
-//        add(buttonEnd, BorderLayout.NORTH);
-//        add(buttonStart, BorderLayout.SOUTH); //по сторнам света
-//        setLayout(new GridLayout(15,15));
-//        for (int i = 0; i < 30; i++){
-//            new Button("Texet " + i);
-
-
-
-        }
-
-
-    // созадание кнопок
-    private void prepareGameButtons() {
-        buttonStartGame = new JButton("Start");
-        buttonEndGame = new JButton("Exit");
+        setVisible(true);
     }
 
-    // все настройки
-    private void prepareGameControlsPanel() {
-        panelControls = new JPanel();
-        panelControls.setLayout(new GridLayout(6, 1));
-
-        // добавляем кнопки
-        panelControls.add(buttonStartGame);
-        panelControls.add(buttonEndGame);
-    }
-
-
-    private  void prepareWindow(){
-
-        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE); //закрыть окно на крестик
-
+    private void prepareWindow() {
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         this.winWidht = 1024;
-        this.winHeight = 780;
-
-        this.winPosX= 300;
+        this.winHeight = 768;
+        this.winPosX = 300;
         this.winPosY = 150;
-
-        setSize(winWidht, winHeight); //изменить размер
+        setSize(winWidht, winHeight);
         setLocation(winPosX, winPosY);
-        setTitle("tic-tac-toe"); // название окна
+        setTitle("Application");
+        setResizable(false);
     }
 
     private void prepareGameSettingsPanel() {
         panelSettings = new JPanel();
-        panelSettings.setLayout(new GridLayout(2,1));
+        panelSettings.setLayout(new GridLayout(2, 1));
     }
 
-}
+    private void prepareGameButtons() {
+        buttonStartGame = new JButton("ЗАПУСТИТЬ ИГРУ");
+        buttonStartGame.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                gameLog.setText("");
+                collectUserSettingsAndLaunch();
 
+            }
+        });
+
+        buttonEndGame = new JButton("ВЫЙТИ ИЗ ИГРЫ ");
+        buttonEndGame.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+
+        });
+
+        buttonClearLog = new JButton("ОЧИСТИТЬ ПОЛЕ");
+        buttonClearLog.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                gameLog.setText("");
+            }
+        });
+    }
+
+    private void prepareGameControlsPanel() {
+        panelControls = new JPanel();
+        panelControls.setLayout(new GridLayout(7, 1));
+
+        labelMapSize = new JLabel(MAP_SIZE_PREFIX + minMapSize);
+        sliderMapSize = new JSlider(minMapSize, maxMapSize, minMapSize);
+
+        //изменение бегунка
+        sliderMapSize.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                int currentPos = sliderMapSize.getValue();
+                labelMapSize.setText(MAP_SIZE_PREFIX + currentPos);
+                sliderWinLength.setMaximum(currentPos);
+            }
+        });
+
+        labelWinLength = new JLabel(WIN_LENGTH_PREFIX + minWinLength);
+        sliderWinLength = new JSlider(minWinLength, minMapSize, minWinLength);
+        sliderWinLength.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                labelWinLength.setText(WIN_LENGTH_PREFIX + sliderWinLength.getValue());
+            }
+        });
+
+        panelControls.add(buttonStartGame);
+        panelControls.add(buttonEndGame);
+        panelControls.add(buttonClearLog);
+        panelControls.add(labelMapSize);
+        panelControls.add(sliderMapSize);
+        panelControls.add(labelWinLength);
+        panelControls.add(sliderWinLength);
+    }
+    //скрол ипанель с тестом
+    private void prepareGameLog() {
+        gameLog = new JTextArea();
+        scrollPanel = new JScrollPane(gameLog);
+        gameLog.setEditable(false);
+        gameLog.setLineWrap(true);
+    }
+
+    //сообщения
+    void saveLog(String str) {
+        gameLog.append(str + "\n");
+    }
+
+
+    //СБОР ДАННЫХ
+    private void collectUserSettingsAndLaunch() {
+        int mapSize = sliderMapSize.getValue();
+        int winLen = sliderWinLength.getValue();
+
+        saveLog("ЗАПУСТИТЬ ИГРУ");
+        saveLog("ВЫБЕРИТИ РАЗМЕРЫ ПОЛЯ " + mapSize + "x" + mapSize);
+        saveLog("КОЛЛИЧЕСТВО ЯЧЕЕК ДЛЯ ПОБЕДЫ " + winLen);
+
+
+        gameField.startGame(mapSize, mapSize, winLen);
+    }
+}
